96543aa55ff8f85a75600eb50f7148465336b84d
Raghvendra <raghvendra.pateriya@gmail.com>
2021-12-31 17:09:11 +0530

    updated driver code

--- implement_python_dict.py
+++ implement_python_dict.py
@@ -6,25 +6,34 @@ Implementing Python Dictionary using hash function without collision handling.
 class HashTable:
   
   def __init__(self):
-    MAX_SIZE = 100
-    self.arr = [None] * MAX_SIZE
+    self.MAX_SIZE = 100
+    self.arr = [None] * self.MAX_SIZE
   
   def get_hash(self, key):
     h = 0
     for character in key:
       h += ord(character)
-    return h % MAX_SIZE
+    return h % self.MAX_SIZE
   
   def __setitem__(self, key, val):
-    index = get_hash(key)
+    index = self.get_hash(key)
     self.arr[index] = val
 
   def __getitem__(self, key):
-    index = get_hash(key)
+    index = self.get_hash(key)
     return self.arr[index]
   
   def __delitem__(self, key):
-    index = get_hash(key)
+    index = self.get_hash(key)
     self.arr[index] = None
-    
-    
+
+# Driver Coded
+
+if __name__ == "__main__":
+    dictionary = HashTable()
+    dictionary["age"] = 5 # Add value
+    dictionary["name"] = "Raghu" # Add more value
+    print(dictionary.arr) # See the Hash table
+    print(dictionary["name"]) # print "Raghu"
+    del dictionary['age'] # delete age
+    print(dictionary.arr) # See the Hash table
