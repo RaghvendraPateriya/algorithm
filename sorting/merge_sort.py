b5c8c9ec6e691896aa56da98e1b8380fbb51460d
Pateriya, Raghvendra <rpateriy@qti.qualcomm.com>
2022-02-09 22:47:09 +0530

    Added merge sort algo

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -9,28 +9,35 @@ Time  : O(nlogn)
 Space : 
 """
 
-def merge_sort(arr):
-  if len(arr) <= 1:
-    retutn arr
-  mid = len(arr)//2
-  left = merge_sort(arr[0:mid])
-  right = merge_sort(arr[mid])
-  return merge(left, right)
 
 def merge(left, right):
-  sorted_arr = []
-  i, j = 0
+  sorted_arr = [] 
+  i = 0
+  j = 0
   while i<len(left) and j<len(right):
     if left[i] < right[j]:
-      sorted_arr.append[i]
+      sorted_arr.append(left[i])
+      i+=1
     else:
-      sorted_arr.append[j]
-    while i<len(left):
-      sorted_arr.append[i]
-    while j<len(right):
-      sorted_arr.append[j]
-  retun sorted_arr
-      
+      sorted_arr.append(right[j])
+      j+=1
+  while i<len(left):
+      sorted_arr.append(left[i])
+      i+=1
+  while j<len(right):
+      sorted_arr.append(right[j])
+      j+=1
+    
+  return sorted_arr
+
+def merge_sort(arr):
+  if len(arr) == 1:
+    return arr
+  mid = len(arr)//2
+  left = merge_sort(arr[:mid])
+  right = merge_sort(arr[mid:])
+  return merge(left, right)
+
 if __name__ == '__main__':
   arr = [1, 10, 30, 4, 8, 6]
-  print(f'Sorted: {merge_sort(arr)}) 
+  print(f'Sorted: {merge_sort(arr)}') 
