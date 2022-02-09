f1a13fb6dece3ec22698469b294523c87f9d8abf
Raghvendra <raghvendra.pateriya@gmail.com>
2022-02-09 15:58:22 +0530

    Added documentation

--- heap/get_k_largest_number.py
+++ heap/get_k_largest_number.py
@@ -1,3 +1,20 @@
+"""
+Find the kth largest element in a number stream
+Design a class to efficiently find the Kth largest element in a stream of numbers. The class should have the following two things:​
+
+The constructor of the class should accept an integer array containing initial numbers from the stream and an integer K.
+
+The class should expose a function add(int num) which will store the given number and return the Kth largest number.
+
+Example:
+
+Input: [4, 1, 3, 12, 7, 14], K = 3
+1. Calling add(6) should return '7'.
+2. Calling add(13) should return '12'.
+2. Calling add(4) should still return '12'.
+
+"""
+
 import heapq
 
 class KLargestNumber:
