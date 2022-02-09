58f6f18d0b9d2e824a3d228e0b91eb84b53449df
Pateriya, Raghvendra <rpateriy@qti.qualcomm.com>
2022-02-09 16:10:12 +0530

    Code improvement

--- heap/get_k_largest_number.py
+++ heap/get_k_largest_number.py
@@ -1,20 +1,25 @@
 import heapq
 
-class KLargestNumber:
-
+class KthLargestNumber:
+  minHeap = []
+  
   def __init__(self, nums, k):
-    self.heap = []
+    
     self.k = k
     for num in nums:
       self.add(num)
 
   def add(self, num):
-     heapq.heappush(self.heap, num)
+     heapq.heappush(self.minHeap, num)
+     
 
-     if len(self.heap) > self.k:
-       heapq.heappop(self.heap)
-     return self.heap[0]
+     if len(self.minHeap) > self.k:
+       heapq.heappop(self.minHeap)
+     print(f'adding num {self.minHeap}')
+     return self.minHeap[0]
 
 if __name__ == '__main__':
-  obj = KLargestNumber([4, 1, 3, 12, 7, 14], 3)
-  print(f'3 rd largest number: {obj.add(6)}')
+  obj = KthLargestNumber([3, 1, 5, 12, 2, 11], 4)
+  print("4th largest number is: " + str(obj.add(6)))
+  print("4th largest number is: " + str(obj.add(13)))
+  print("4th largest number is: " + str(obj.add(4)))
