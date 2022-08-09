1eb85a2e68f6f406c77fef3a2e25c95928f3fcb0
Raghvendra <raghvendra.pateriya@gmail.com>
2022-08-09 10:29:23 +0530

    Added boundary conditions

--- fibonacci.py
+++ fibonacci.py
@@ -6,17 +6,29 @@ Output: 0 1 1 2 3 5
 
 """
 
-def get_fibonacci_series(n):
-  result_list = []
-  current_element = 0
-  next_element = 1
-  counter = 0
-  result_list.append(current_element)
-  result_list.append(next_element)
-  while(counter<=n-2):
-    current_element, next_element = next_element, current_element + next_element
+def get_fibonacci_series(nth_term):
+    result_list = []
+    current_element = 0
+    next_element = 1
+    counter = 0
+    result_list.append(current_element)
     result_list.append(next_element)
-    counter += 1
-  return  result_list
 
-print(get_fibonacci_series(5))
+    if nth_term <= 0:
+        return "Please enter positive number"
+    if nth_term == 1:
+        return current_element
+
+    while (counter < nth_term - 2):
+        current_element, next_element = next_element, current_element + next_element
+        result_list.append(next_element)
+        counter += 1
+    return result_list
+
+  
+# Driver Code
+if __name__ == '__main__':
+    print(get_fibonacci_series(3))
+    #print(get_fibonacci_series(0))
+    #print(get_fibonacci_series(-1))
+    #print(get_fibonacci_series(1)) 
