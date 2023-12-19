2cd824bbf8800ab3add35b2266180a19a32ada57
Raghvendra <raghvendra.pateriya@gmail.com>
2023-12-19 16:30:58 +0530

    Created get_maximum_mex_of_subarray.py

---
+++ sliding_window/get_maximum_mex_of_subarray.py
@@ -0,0 +1,12 @@
+"""
+Get the maximum MEX from all subarrays of length K
+MEX: Smallest positive integer not present in array
+Input: [3, 2, 1, 4], K=2
+Output: 3
+
+Explanation:
+Window of K size | The Smallest integer not present in array 
+[3, 2]           |  1
+[2, 1]           |  3 # This would the desire result
+[1, 4]           |  2
+"""
